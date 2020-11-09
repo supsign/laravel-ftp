852f54ba2b5baf6be937d33fafb7007aabee015b
Florian Ratz <florian.ratz@supsign.ch>
2020-11-09 09:46:56 +0100

    add service provider and namespaces

--- src/FtpConnector.php
+++ src/FtpConnector.php
@@ -1,7 +1,6 @@
 <?php
 
-namespace App\Helpers;
-
+namespace Supsign\LaravelFtpConnector;
 
 class FtpConnector {
     protected 
---
+++ src/LaravelFtpConnectorServiceProvider.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace Supsign\LaravelFtpConnector;
+
+use Illuminate\Support\ServiceProvider;
+
+class LaravelFtpConnectorServiceProvider extends ServiceProvider
+{
+
+    public function boot() 
+    {
+
+    }
+
+    public function register()
+    {
+
+    }
+}
